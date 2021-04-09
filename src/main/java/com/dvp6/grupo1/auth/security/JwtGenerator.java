1a15252db953bbacf5491c677888631d234ce6e3
guirubio <guilherme.rubio@outlook.com.br>
2021-04-08 23:27:09 -0300

    Removido as Clains no JWT e adicionado um dump

--- src/main/java/com/dvp6/grupo1/auth/security/JwtGenerator.java
+++ src/main/java/com/dvp6/grupo1/auth/security/JwtGenerator.java
@@ -1,7 +1,5 @@
 package com.dvp6.grupo1.auth.security;
 
-import java.util.Date;
-
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
@@ -12,28 +10,21 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 public class JwtGenerator {
 
-    private JwtGenerator(){
+    private JwtGenerator() {
     }
 
     public static String getJWT() throws JWTCreationException {
 
-        Date date = new Date();
-        String role = null;
-        String expiresAt = null;
         String token = null;
         String username = null;
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
             username = authentication.getName();
-            role = authentication.getAuthorities().toString();
         }
-       
-        role = role.substring(1, role.length()-1);
-        expiresAt = Long.toString(date.getTime() + 600000);
 
         Algorithm algorithm = Algorithm.HMAC256("secret");
-        token = JWT.create().withIssuer(username).withClaim("role", role).withClaim("exp", expiresAt).sign(algorithm);
+        token = JWT.create().withIssuer(username).sign(algorithm);
 
         return token;
     }
