43a20242691827430cdaa7002258ed8a64c88088
guirubio <guilherme.rubio@outlook.com.br>
2021-04-26 15:05:24 -0300

    Ajustes nas rotas

--- src/main/java/com/dvp6/grupo1/auth/controller/AuthController.java
+++ src/main/java/com/dvp6/grupo1/auth/controller/AuthController.java
@@ -13,6 +13,8 @@ import com.dvp6.grupo1.auth.security.JwtGenerator;
 import java.sql.SQLException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,33 +27,38 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
   /*
-    Instancia a classe do repositório do JPA.  
-  */
+   * Instancia a classe do repositório do JPA.
+   */
   @Autowired
   private AuthRepository authRepository;
 
   /*
-    Criando a rota /login com o verbo GET.
-    Responsável por receber o usuário e senha e retornar um token jwt para o usuário.
-  */
+   * Criando a rota /login com o verbo GET. Responsável por receber o usuário e
+   * senha e retornar um token jwt para o usuário.
+   */
   @GetMapping("/login")
   public String login() throws SQLException {
     return JwtGenerator.getJwtToken();
   }
 
   /*
-    Criando a rota /addUser com o verbo POST.
-    Recebe um Json com os dados e cria um novo usuário no sistema.
-  */
+   * Criando a rota /addUser com o verbo POST. Recebe um Json com os dados e cria
+   * um novo usuário no sistema.
+   */
   @PostMapping("/addUser")
-  public void addUser(@RequestBody AuthEntity authEntity) {
-    authRepository.save(authEntity);
+  public ResponseEntity<String> addUser(@RequestBody AuthEntity authEntity) {
+    try {
+      authRepository.save(authEntity);
+      return new ResponseEntity<>("Usuário adicionado com sucesso!", HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>("Erro: por favor valide se o usuário já existe.", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   /*
-    Criando a rota /getUser com o verbo GET
-    Retorna a lista com todos os usuários.
-  */
+   * Criando a rota /getUser com o verbo GET Retorna a lista com todos os
+   * usuários.
+   */
   @GetMapping("/getUsers")
   public Iterable<AuthEntity> getUsers() {
     return authRepository.findAll();
--- src/main/java/com/dvp6/grupo1/auth/security/AuthSecurityConfig.java
+++ src/main/java/com/dvp6/grupo1/auth/security/AuthSecurityConfig.java
@@ -12,10 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 
 /*
   Classe responsável pela configuração do Spring Security.
@@ -24,31 +24,40 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 public class AuthSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    /*
-      Instanciando o data source para consultar o usuário e senha do banco de dados.
-    */
-    @Autowired
-    private DataSource dataSource;
+  /*
+   * Instanciando o data source para consultar o usuário e senha do banco de
+   * dados.
+   */
+  @Autowired
+  private DataSource dataSource;
 
-    /*
-      Método responsável por validar o usuário e senha.
-    */
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder()).dataSource(dataSource)
-                .usersByUsernameQuery("SELECT username, password, enabled from `auth-db`.auth where username =?")
-                .authoritiesByUsernameQuery("SELECT username, role FROM `auth-db`.auth WHERE username =?");
+  /*
+   * Método responsável por validar o usuário e senha.
+   */
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder()).dataSource(dataSource)
+        .usersByUsernameQuery("SELECT username, password, enabled from `auth-db`.auth where username =?")
+        .authoritiesByUsernameQuery("SELECT username, role FROM `auth-db`.auth WHERE username =?");
 
-    }
+  }
 
-    /*
-      Método responsável filtar as permissões para as rotas.
-    */
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().httpBasic();
-        http.authorizeRequests().antMatchers("/getToken").permitAll().antMatchers("/getUsers").permitAll()
-                .antMatchers("/addUser").hasAuthority("ADMIN").anyRequest().authenticated();
+  /*
+   * Método responsável por deixar a documentação anomina.
+   */
+  @Override
+  public void configure(WebSecurity http) throws Exception {
+    http.ignoring().antMatchers("/swagger-auth");
+  }
 
-    }
+  /*
+   * Método responsável filtar as permissões para as rotas.
+   */
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable().httpBasic();
+    http.authorizeRequests().antMatchers("/login").permitAll().antMatchers("/getUsers").permitAll()
+        .antMatchers("/addUser").hasAuthority("ROLE_ADMIN").anyRequest().authenticated();
+
+  }
 }
