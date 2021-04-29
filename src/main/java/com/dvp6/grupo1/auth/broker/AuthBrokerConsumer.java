362ccc6b7382f5a1f952f8bfc10e61e6965bb5ba
guirubio <guilherme.rubio@outlook.com.br>
2021-04-29 13:14:44 -0300

    ajustes

---
+++ src/main/java/com/dvp6/grupo1/auth/broker/AuthBrokerConsumer.java
@@ -0,0 +1,38 @@
+/*
+  Autor: Guilherme Rubio
+  e-mail: guilherme.rubio@outlook.com.br
+  Data: 14/04/2021
+*/
+
+package com.dvp6.grupo1.auth.broker;
+
+import com.dvp6.grupo1.auth.model.AuthEntity;
+import com.dvp6.grupo1.auth.model.AuthRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/*
+  Classe responsável por consumir as filas no RabbitMQ.
+*/
+@Component
+public class AuthBrokerConsumer {
+
+    @Autowired
+    private AuthRepository authRepository;
+    
+    /*
+      Método para ler a fila e salvas os dados no banco.
+    */
+    @RabbitListener(queues = AuthBrokerConfig.QUEUE)
+    public void consumer(Message message) throws JsonMappingException, JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();        
+        AuthEntity authEntity = mapper.readValue(new String(message.getBody()), AuthEntity.class);
+        authRepository.save(authEntity);
+    }
+}
--- src/main/java/com/dvp6/grupo1/auth/controller/AuthController.java
+++ src/main/java/com/dvp6/grupo1/auth/controller/AuthController.java
@@ -12,12 +12,9 @@ import com.dvp6.grupo1.auth.security.JwtGenerator;
 
 import java.sql.SQLException;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
@@ -27,10 +24,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
   /*
-   * Instancia a classe do repositório do JPA.
+   * Criando a rota / com o verbo GET.
    */
-  @Autowired
-  private AuthRepository authRepository;
+  @GetMapping("/")
+  public ResponseEntity<String> home() {
+    return new ResponseEntity<>("Ok!", HttpStatus.OK);
+  }
 
   /*
    * Criando a rota /login com o verbo GET. Responsável por receber o usuário e
@@ -39,29 +38,6 @@ public class AuthController {
   @GetMapping("/login")
   public String login() throws SQLException {
     return JwtGenerator.getJwtToken();
-  }
-
-  /*
-   * Criando a rota /addUser com o verbo POST. Recebe um Json com os dados e cria
-   * um novo usuário no sistema.
-   */
-  @PostMapping("/addUser")
-  public ResponseEntity<String> addUser(@RequestBody AuthEntity authEntity) {
-    try {
-      authRepository.save(authEntity);
-      return new ResponseEntity<>("Usuário adicionado com sucesso!", HttpStatus.OK);
-    } catch (Exception e) {
-      return new ResponseEntity<>("Erro: por favor valide se o usuário já existe.", HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-  }
-
-  /*
-   * Criando a rota /getUser com o verbo GET Retorna a lista com todos os
-   * usuários.
-   */
-  @GetMapping("/getUsers")
-  public Iterable<AuthEntity> getUsers() {
-    return authRepository.findAll();
   }
 
 }
